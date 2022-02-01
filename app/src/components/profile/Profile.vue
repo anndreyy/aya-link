fda29d57e3bb4c8210e12fb38eb78b8eafde1d74
Anndrey Macedo <anndreyy@live.com>
2022-02-01 06:14:55 -0300

    Add profile mockup

---
+++ app/src/components/profile/Profile.vue
@@ -0,0 +1,37 @@
+<script>
+import ProfileLinks from "./ProfileLinks.vue";
+export default {
+    name: "Profile",
+    props: [],
+    data: () => ({}),
+    components: { ProfileLinks },
+    methods: {},
+    created() { },
+    mounted() { },
+    unmounted() { },
+}
+</script>
+<template>
+    <div id="Profile">
+
+        <div id="profileCover">            
+        </div>
+
+        <img alt="Veronica" src="@/assets/profile-pic.png" id="profilePhoto">
+
+        <div class="profileName">
+            <h1>
+                Verônica
+                <br />Mascarenhas
+            </h1>
+        </div>
+        <div class="profileDesc">
+            <h2>
+                A rainha das batatas
+            </h2>
+        </div>
+
+
+        <ProfileLinks />
+    </div>
+</template>
